3def2a45a36787a89c948a4dcab16f10db4798d1
Suryansh Vishwakarma <suryanshv16@gmail.com>
2026-04-30 23:45:13 +0530

    Add preprocess.py

---
+++ src/preprocessing.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+#Convert date to datetime
+def convert_date(df):
+    df["Date"] = pd.to_datetime(df["Date"])
+    return df
+
+#setting date as index
+def set_index(df):
+    df.set_index("Date", inplace=True)
+    return df
+
+#handle missing values
+def handle_missing_value(df):
+    df.ffill()
+    df.dropna(inplace=True)
+    return df
+
+def preprocess(df):
+    df = convert_date(df)
+    df = set_index(df)
+    df= handle_missing_value(df)
+    return df
