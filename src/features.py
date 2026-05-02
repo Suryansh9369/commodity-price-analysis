c09de5132710a1f64136928b08ab3ad206cc2f32
Suryansh Vishwakarma <suryanshv16@gmail.com>
2026-05-02 19:28:45 +0530

    this script will return dataframe with new feature like returns, volatility(Risk) and moving average.

--- src/features.py
+++ src/features.py
@@ -1,12 +1,15 @@
+#features.py
+
+# this script will return dataframe with new feature like returns, volatility(Risk) and moving average.
 import pandas as pd
 
 #calculate returns
 def calculate_return(df):
-    return df.pct_change()
+    return df.pct_change(fill_method=None)
 
 #calculate volatility(Risk) or rolling window of 30 days
 def calculate_volatility(df, window=30):
-    return df.pct_change().rolling(window).std()
+    return df.pct_change(fill_method=None).rolling(window).std()
 
 # calculating moving averages
 def moving_average(df, window=30):
@@ -15,10 +18,16 @@ def moving_average(df, window=30):
 # Master feature function to be called.
 
 def create_features(df):
-    df["Returns"] = calculate_return(df)
-    df["Volatility"] = calculate_volatility(df,window=30)
-    df["Moving_Average_30_Days"] = moving_average(df, window=30)
+    returns = calculate_return(df)
+    volatility = calculate_volatility(df)
+    ma = moving_average(df)
+    
+    # Rename columns properly
+    returns.columns = [col + "_Returns" for col in df.columns]
+    volatility.columns = [col + "_Volatility" for col in df.columns]
+    ma.columns = [col + "_MA30" for col in df.columns]
+    
+    df = pd.concat([df, returns, volatility, ma], axis=1)
     
     return df
 
-# this script will return dataframe with new feature like returns, volatility(Risk) and moving average.
